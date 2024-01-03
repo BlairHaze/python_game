8c3a5b3e733c6180ce3ad350aad4403a8b3b7754
BlairHaze <126704689+BlairHaze@users.noreply.github.com>
2024-01-03 13:16:55 +0100

    Test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! Kocham naleśniki i kotki!'
 
 if __name__ == '__main__':
     app.run(debug=True)

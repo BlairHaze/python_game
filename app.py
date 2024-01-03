9c2cdd4aac065a578ce53316328d8b45fbc1d208
BlairHaze <126704689+BlairHaze@users.noreply.github.com>
2024-01-03 18:57:35 +0100

    Dodano tutorial stuff

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World! Kocham naleśniki i kotki!'
+def homepage():
+    return "<h1>Zacznij swoją epicką przygodę!</h1> <em>Hello, World!</em> <strong>Kocham naleśniki i kotki!</strong>"
+
+@app.route('/<name>')
+def name(name):
+    return f"Witaj {name}!"
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for("homepage"))
 
 if __name__ == '__main__':
     app.run(debug=True)
